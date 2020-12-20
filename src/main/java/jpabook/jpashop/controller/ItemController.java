f6893bc8c5c8b652be585c5aaeac011d40148149
Lee SangHyup <bosuksh@gmail.com>
2020-12-20 17:34:58 +0900

    [add] 상품조회 추가

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -9,22 +9,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/items")
 public class ItemController {
   private final ItemService itemService;
 
-  @GetMapping("/items/new")
+  @GetMapping("/new")
   public String createForm(Model model){
    model.addAttribute("itemForm", new BookForm());
    return "items/createItemForm";
   }
 
-  @PostMapping("/items/new")
+  @PostMapping("/new")
   public String create(BookForm bookForm) {
     Book book = bookForm.createBook();
     itemService.saveItem(book);
     return "redirect:/";
+  }
+
+  @GetMapping
+  public String list(Model model){
+    List<Item> items = itemService.findItems();
+    model.addAttribute("items", items);
+    return "items/itemList";
   }
 }
